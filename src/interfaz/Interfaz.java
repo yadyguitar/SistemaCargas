e3ac7a988f00db6205083d3ef6ffb691529da223
Yadira Fleitas <yadyguitar@yahoo.es>
2016-10-19 12:38:25 -0500

    Primer commit

---
+++ src/interfaz/Interfaz.java
@@ -0,0 +1,25 @@
+package interfaz;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Interfaz extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent mainLayout = FXMLLoader.load(getClass().getResource("InterfazFXML.fxml"));
+        Scene scene = new Scene(mainLayout, 800, 600);
+        primaryStage.setTitle("Sistema de Cargas");
+        primaryStage.setScene(scene);
+        primaryStage.show();	
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
