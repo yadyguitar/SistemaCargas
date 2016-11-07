05952d801355ea9dbfdc24436cd5b2da2375f4c0
Yadira Fleitas <yadyguitar@yahoo.es>
2016-11-07 13:52:02 -0600

    interfaz casi lista

--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -7,16 +7,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
 
 
 public class Interfaz extends Application {
 	@Override
-	public void start(Stage primaryStage) throws IOException {
+	public void start(Stage primaryStage) throws IOException  {
 		Parent mainLayout = FXMLLoader.load(getClass().getResource("InterfazFXML.fxml"));
         Scene scene = new Scene(mainLayout, 1000, 600);
         primaryStage.setTitle("Sistema de Cargas");
         primaryStage.setScene(scene);
-        primaryStage.show();	
+        Image ico= new Image("interfaz/logo_uv.png");
+        
+        primaryStage.getIcons().add(ico);
+        primaryStage.show();
+        
 	}
 	
 	public static void main(String[] args) {
