989b5aef52720ebaa6b3dc95724732021da16692
Yadira Fleitas <yadyguitar@yahoo.es>
2017-03-28 10:25:31 -0600

    resolver problema de conexión controlador-fxml (:

--- src/controllers/DatosIniciales.java
+++ src/controllers/DatosIniciales.java
@@ -1,68 +1,24 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 public class DatosIniciales implements Initializable {
-
-	static float diametro,altura,area,volumen,pesoAnillo, cteAparato;
-	@FXML TextField diametroFXML,alturaFXML,areaFXML,volumenFXML,pesoAnilloFXML,cteAparatoFXML;
+	@FXML ConstanteEquipo constanteEquipo;
+	
+	
 	static String flanera;
 	static float wm,ws,wf,wp;
 	@FXML TextField flaneraFXML,wmFXML,wsFXML,wfFXML,wpFXML;
 	
 	//Constantes de Equipos
-	public float getDiametro(){
-		diametro= Float.parseFloat(diametroFXML.getText());
-		return diametro;
-	}
-	public void setDiametro(float diametro){
-		diametroFXML.setText(String.format("%.3f",diametro));
-	}
 	
-	public float getAltura(){
-		altura= Float.parseFloat(alturaFXML.getText());
-		return altura;
-	}
-	public void setAltura(float altura){
-		alturaFXML.setText(String.format("%.3f",altura));
-	}
-	
-	public float getArea(){
-		area= Float.parseFloat(areaFXML.getText());
-		return area;
-	}
-	public void setArea(float area){
-		areaFXML.setText(String.format("%.3f",area));
-	}
-	
-	public float getVolumen(){
-		volumen= Float.parseFloat(volumenFXML.getText());
-		return volumen;
-	}
-	public void setVolumen(float volumen){
-		volumenFXML.setText(String.format("%.3f",volumen));
-	}
-
-	public float getPesoAnillo(){
-		pesoAnillo= Float.parseFloat(pesoAnilloFXML.getText());
-		return pesoAnillo;
-	}
-	public void setPesoAnillo(float pesoAnillo){
-		pesoAnilloFXML.setText(String.format("%.3f",pesoAnillo));
-	}
-
-	public float getCteAparato(){
-		cteAparato= Float.parseFloat(cteAparatoFXML.getText());
-		return volumen;
-	}
-	public void setCteAparato(float cteAparato){
-		cteAparatoFXML.setText(String.format("%.3f",cteAparato));
-	}
 	///////////////////////////////////////////////
 	
 	//Humedad Inicial
@@ -111,9 +67,77 @@ public class DatosIniciales implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
 	}
 	
 }
+
+class ConstanteEquipo{
+	
+	float diametro,altura,area,volumen,pesoAnillo, cteAparato;
+	@FXML TextField diametroFXML,alturaFXML,areaFXML,volumenFXML,pesoAnilloFXML,cteAparatoFXML;
+	
+	/*public ConstanteEquipo(){
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+				"DatosIniciales_ConstanteEquipo.fxml"));
+				        fxmlLoader.setRoot(this);
+				        fxmlLoader.setController(this);
+
+				        try {
+				            fxmlLoader.load();
+				        } catch (IOException exception) {
+				            throw new RuntimeException(exception);
+				        }
+	}*/
+	
+	public float getDiametro(){
+		diametro= Float.parseFloat(diametroFXML.getText());
+		return diametro;
+	}
+	public void setDiametro(float diametro){
+		diametroFXML.setText(String.format("%.3f",diametro));
+	}
+	
+	public float getAltura(){
+		altura= Float.parseFloat(alturaFXML.getText());
+		return altura;
+	}
+	public void setAltura(float altura){
+		alturaFXML.setText(String.format("%.3f",altura));
+	}
+	
+	public float getArea(){
+		area= Float.parseFloat(areaFXML.getText());
+		return area;
+	}
+	public void setArea(float area){
+		areaFXML.setText(String.format("%.3f",area));
+	}
+	
+	public float getVolumen(){
+		volumen= Float.parseFloat(volumenFXML.getText());
+		return volumen;
+	}
+	public void setVolumen(float volumen){
+		volumenFXML.setText(String.format("%.3f",volumen));
+	}
+
+	public float getPesoAnillo(){
+		pesoAnillo= Float.parseFloat(pesoAnilloFXML.getText());
+		return pesoAnillo;
+	}
+	public void setPesoAnillo(float pesoAnillo){
+		pesoAnilloFXML.setText(String.format("%.3f",pesoAnillo));
+	}
+
+	public float getCteAparato(){
+		cteAparato= Float.parseFloat(cteAparatoFXML.getText());
+		return volumen;
+	}
+	public void setCteAparato(float cteAparato){
+		cteAparatoFXML.setText(String.format("%.3f",cteAparato));
+	}
+	
+}
+
 
 
